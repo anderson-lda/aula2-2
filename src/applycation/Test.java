ee44df688ab0c11ae5901dd1521682bf716448fb
Anderson Lima <anderson.lima.araujo@hotmail.com>
2020-06-15 00:57:22 -0300

    Created project

---
+++ src/applycation/Test.java
@@ -0,0 +1,10 @@
+package applycation;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Strawberry milkshake");
+	}
+
+}
